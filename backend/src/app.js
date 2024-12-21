003530ad99e8a31f7e97be0cc1f837d0725cf522
Noel1903 <osmar19lemus@gmail.com>
2024-12-21 00:06:11 -0600

    [ADD] Usuarios controlador

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,17 @@ const cors = require('cors');
 require('dotenv').config();
 
 const ProductsRoutes = require('./routes/ProductsRoutes');
+const UsersRoutes = require('./routes/UsersRoutes');
+
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 
 app.use('/', ProductsRoutes);
+app.use('/', UsersRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
--- backend/src/models/UsersModel.js
+++ backend/src/models/UsersModel.js
@@ -1,14 +1,27 @@
 const connectDatabase = require('../config/db.js')
 
-const createUserSystem = async (id_rol,email,name,password,phone,date_birth) => {
+const createUserSystem = async (id_rol,id_state,email,name,password,phone,date_birth,date) => {
     try {
-        const query = `EXEC createUser @rol_idRol = ${id_rol}, @correo_electronico = '${email}', @nombre_completo = '${name}', @password = '${password}', @telefono = '${phone}', @fecha_nacimiento = '${date_birth}'`;
+        const query = `EXEC createUser @rol_idRol = ${id_rol},@estado_idEstados = '${id_state}', @correo_electronico = '${email}', @nombre_completo = '${name}', @password = '${password}', @telefono = '${phone}', @fecha_nacimiento = '${date_birth} ,@fecha_creacion = '${date}'`;
         const pool = await connectDatabase();
         const result = await pool.request().query(query);
         pool.close();
         return result['recordset'][0][''];
     }catch(error){
         console.error('Error al crear el usuario', error);
+    }
+}
+
+
+const modifyUserSystem = async (id_usuario,id_rol,id_estado,email,name,password,phone,date_birth,date) => {
+    try {
+        const query = `EXEC modifyUser @idUsuarios = ${id_usuario}, @rol_idRol = ${id_rol},@estados_idEstados = '${id_estado}', @correo_electronico = '${email}', @nombre_completo = '${name}', @password = '${password}', @telefono = '${phone}', @fecha_nacimiento = '${date_birth}`;
+        const pool = await connectDatabase();
+        const result = await pool.request().query(query);
+        pool.close();
+        return result['recordset'][0][''];
+    }catch(error){
+        console.error('Error al modificar el usuario', error);
     }
 }
 
@@ -25,6 +38,18 @@ const createClientSystem = async (razon_social,nombre_comercial,direccion_entreg
 }
 
 
+const modifyClientSystem = async (id_cliente,razon_social,nombre_comercial,direccion_entrega,telefono,email) => {
+    try {
+        const query = `EXEC modifyClient @idClientes = ${id_cliente}, @razon_social = '${razon_social}', @nombre_comercial = '${nombre_comercial}', @direccion_entrega = '${direccion_entrega}', @telefono = '${telefono}', @email = '${email}'`;
+        const pool = await connectDatabase();
+        const result = await pool.request().query(query);
+        pool.close();
+        return result['recordset'][0][''];
+    }catch(error){
+        console.error('Error al modificar el cliente', error);
+    }
+}   
 
 
-module.exports = { createUserSystem, createClientSystem };
+
+module.exports = { createUserSystem, createClientSystem, modifyUserSystem, modifyClientSystem };
